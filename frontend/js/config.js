3437c294834b4ad25200857e54332c47105b0b84
nadavra <42138073+nadavra@users.noreply.github.com>
2018-10-30 18:21:11 +0200

    Add files via upload

---
+++ frontend/js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_cJy9crTXh', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '7tb0ur0n2k8im6vq73rq3v95q9' //is this used anywhere?
+    },
+};
