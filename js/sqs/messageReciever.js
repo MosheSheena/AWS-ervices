a4b80bb7370deac78f26710f2bb9c98a373196c1
amitlevy21 <amitlevy347@gmail.com>
2018-11-30 15:49:15 +0200

    small

--- js/sqs/messageReciever.js
+++ js/sqs/messageReciever.js
@@ -1,5 +1,4 @@
 var AWS = require('aws-sdk');
-var dynamoDB = require('../dynamoDB/dynamo');
 AWS.config.update({
     region: 'us-east-1'
 });
@@ -7,6 +6,30 @@ AWS.config.update({
 var sqs = new AWS.SQS();
 
 var queueURL = 'https://sqs.us-east-1.amazonaws.com/821023756204/test_queue';
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+
+function recordTransaction(transaction) {
+    const params = {
+        TableName: 'Transactions',
+        Item: {
+            'id': transaction.id,
+            'serviceID': transaction.serviceID,
+            'quantity': transaction.quantity,
+            'providerUN': transaction.providerUN,
+            'consumerUN': transaction.consumerUN,
+            'dateCreated': transaction.dateCreated
+        }
+    };
+
+    return ddb.put(params, function (err, data) {
+        if (err) {
+            console.error('Unable to add transaction. Error JSON:', JSON.stringify(err, null, 2));
+        } else {
+            console.log('PutItem succeeded ', JSON.stringify(data));
+        }
+    }).promise();
+}
 
 function listenForSQS() {
     var params = {
@@ -16,34 +39,29 @@ function listenForSQS() {
         QueueUrl: queueURL,
         VisibilityTimeout: 20,
         WaitTimeSeconds: 10
-       };
+    };
 
-       sqs.receiveMessage(params, function(recvErr, recvData) {
-         if (recvErr) {
-           console.log('Receive Error', recvErr);
-         } else if (recvData.Messages) {
-           var currentMessage = recvData.Messages[0];
-           var messageAttributes = currentMessage.MessageAttributes;
-           var messageType = messageAttributes.messageType;
-           if (messageType == 'service') {
-            dynamoDB.recordService(messageAttributes.Body);
-           }
-           else {
-            dynamoDB.recordTransaction(messageAttributes.Body);
-           }
-           var deleteParams = {
-             QueueUrl: queueURL,
-             ReceiptHandle: recvData.Messages[0].ReceiptHandle
-           };
-           sqs.deleteMessage(deleteParams, function(delErr, delData) {
-             if (delErr) {
-               console.log('Delete Error', delErr);
-             } else {
-               console.log('Message Deleted', delData);
-             }
-           });
-         }
-       });
+    sqs.receiveMessage(params, function (recvErr, recvData) {
+        if (recvErr) {
+            console.log('Receive Error', recvErr);
+        } else if (recvData.Messages) {
+            var currentMessage = recvData.Messages[0];
+            recordTransaction(JSON.parse(currentMessage.Body)).then(() => {
+                var deleteParams = {
+                    QueueUrl: queueURL,
+                    ReceiptHandle: recvData.Messages[0].ReceiptHandle
+                };
+                sqs.deleteMessage(deleteParams, function (delErr, delData) {
+                    if (delErr) {
+                        console.log('Delete Error', delErr);
+                    } else {
+                        console.log('Message Deleted', delData);
+                    }
+                });
+            });
+
+        }
+    });
 }
 
 listenForSQS();
